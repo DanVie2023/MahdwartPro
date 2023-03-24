02f5ebec8b8607db8d5f0033076b914a5778920f
DanVie2023 <Daniel.Vieler@web.de>
2023-03-24 14:40:05 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mahdwartpro/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mahdwartpro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
